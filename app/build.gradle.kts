88a21b1213b41bd93df96267038b52680381f680
Patrick Gawron <mail@patrickgawron.com>
2026-06-12 09:22:29 +0200

    chore: bump to v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.meshwalkie"
         minSdk = 29        // MediaCodec software Opus encoder requires API 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildTypes {
